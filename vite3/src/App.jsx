80334c28b35400cccc2e8382bae21ef18ae660ac
Abdullah-9864 <abdullahshahid9864@gmail.com>
2026-05-05 01:21:49 +0500

    files added

--- vite3/src/App.jsx
+++ vite3/src/App.jsx
@@ -1,16 +1,37 @@
 // import React from 'react'
-
+import Card from './components/Card'
 const App = () => {
   return (
     <div className="parent">
-
-      <div  className="card">
-        <img src="https://images.unsplash.com/photo-1773332585749-5146862ba746?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1N3x8fGVufDB8fHx8fA%3D%3D" alt="image" />
+        {/* <div className="card">
+        <img src="https://plus.unsplash.com/premium_photo-1669312732419-a55959a56e6c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0OHx8fGVufDB8fHx8fA%3D%3D" alt="image" />
         <h1>unsplash images</h1>
-        <p>These are some beautiful images from Unsplash.</p>
-        <button>Learn More</button>
-      </div>
-      
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <button>view profile</button>
+        </div>
+
+
+
+        <div className="card">
+        <img src="https://plus.unsplash.com/premium_photo-1669312732419-a55959a56e6c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0OHx8fGVufDB8fHx8fA%3D%3D" alt="image" />
+        <h1>unsplash images</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <button>view profile</button>
+        </div>
+
+
+
+        <div className="card">
+        <img src="https://plus.unsplash.com/premium_photo-1669312732419-a55959a56e6c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0OHx8fGVufDB8fHx8fA%3D%3D" alt="image" />
+        <h1>unsplash images</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <button>view profile</button>
+        </div> */}
+
+        <div>
+        <Card user="Abdullah" img="https://images.unsplash.com/photo-1773332585815-f106a5d6ed6c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8"/>
+        <Card user="Asadullah" img="https://images.unsplash.com/photo-1776675456831-95ccdaa8c907?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D"/>
+        </div>
     </div>
   )
 }
--- vite3/src/components/Card.jsx
+++ vite3/src/components/Card.jsx
@@ -1,6 +1,18 @@
-function Card()
-{
-    return "hi im a card";
+// import React from 'react'
+
+const Card = (props) => {
+    // console.log(props);
+  return (
+    <div>
+        <div className="card">
+        <img src={props.img} alt="image" />
+        <h1>{props.user}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <button>view profile</button>
+        </div>
+      
+    </div>
+  )
 }
 
-export default Card;
+export default Card
