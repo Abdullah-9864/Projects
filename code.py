dfebd1335dadfeaae4298d06850ddcf2b02d9f4e
Abdullah-9864 <abdullahshahid9864@gmail.com>
2026-05-05 19:44:53 +0500

    files added

---
+++ code.py
@@ -0,0 +1,16 @@
+print("python is fun!")
+print("i am", 34)
+
+x,y,z="abdullah", "mohammad", "ali"
+print(x)
+print(y)    
+print(z)
+
+
+localVariable="this is a local variable"
+
+def myFunction():
+    print("this is a function")
+    print(localVariable)
+    
+myFunction()
