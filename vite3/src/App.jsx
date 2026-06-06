ab0ed3dd53f06cfacca897f292d3473ae696fea0
Abdullah-9864 <abdullahshahid9864@gmail.com>
2026-06-06 10:35:16 +0500

    files added

--- vite3/src/App.jsx
+++ vite3/src/App.jsx
@@ -4,7 +4,8 @@ import Header from './components/header/header'
 
 const App = () => {
   return (
-    <div>
+    <div className='bg-red-600'>
+      <h1 className='text-white text-2xl font-bold'>Hello this is tailwindcss</h1>
       <Button/>
       <Header/>
       
